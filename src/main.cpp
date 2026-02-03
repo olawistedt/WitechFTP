4aa93ee0874ca2376d54cb787a36391cfa1a6c99
Ola Wistedt <ola@witech.se>
2026-02-03 14:33:45 +0100

    Fixed problem with static variable.

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 #include <QDebug>
 #include <QSslSocket>
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-    qDebug() << "SSL Library Build Version:" << QSslSocket::sslLibraryBuildVersionString();
-    qDebug() << "SSL Library Version:" << QSslSocket::sslLibraryVersionString();
-
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+  QApplication a(argc, argv);
+  qDebug() << "SSL Library Build Version:" << QSslSocket::sslLibraryBuildVersionString();
+  qDebug() << "SSL Library Version:" << QSslSocket::sslLibraryVersionString();
+  MainWindow w;
+  w.show();
+  return a.exec();
 }
